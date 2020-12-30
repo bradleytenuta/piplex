9eab24aff327cec367b4ffd6bfeafa9a988d1c5f
DDairy <26867342+bradleytenuta@users.noreply.github.com>
2020-12-30 17:55:37 +0000

    fixed spelling mistake

--- plex-run-cron.sh
+++ plex-run-cron.sh
@@ -1,6 +1,6 @@
 # Below is the cron job to add to the root crontab file, not the bradleytenuta user.
 # open the crontab and edit it to add this cron job with the command: 'sudo crontab -e'
-# Note 'sudo crontan -e', gives a different crontab to 'crontab -e'.
+# Note 'sudo crontab -e', gives a different crontab to 'crontab -e'.
 #
 # 0 0 * * * sudo /home/bradleytenuta/piplex/plex-run-cron.sh >> /home/bradleytenuta/piplex/logs/plex-run-cron.txt 2>&1
 #
