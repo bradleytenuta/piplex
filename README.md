57616e4fdec9498e3a35f234526dae20ff0b2ca1
DDairy <26867342+bradleytenuta@users.noreply.github.com>
2020-12-30 17:52:39 +0000

    Added a readme file. Updated the run cron script.

---
+++ README.md
@@ -0,0 +1,9 @@
+Once you've checked out this repository, you'll need to give execute permission
+to these shell files as by default, Linux will not let you run them.
+
+The three commands will give the user who owns these files, permission to execute them.
+Run these commands below in the current directory.
+
+chmod u+x plex-install.sh
+chmod u+x plex-run-cron.sh
+chmod u+x plex-update.sh
--- plex-run-cron.sh
+++ plex-run-cron.sh
@@ -1,9 +1,10 @@
-# Below this line is the command for creating a cron job. This cron just will execute this file.
-# The logs of this crobjob will be stored in the logs folder of this directory.
+# Below is the cron job to add to the root crontab file, not the bradleytenuta user.
+# open the crontab and edit it to add this cron job with the command: 'sudo crontab -e'
+# Note 'sudo crontan -e', gives a different crontab to 'crontab -e'.
 #
-# sudo crontab * * * * * /home/bradleytenuta/piplex/plex-run-cron.sh >> /home/bradleytenuta/piplex/logs/plex-run-cron.log 2>&1
+# 0 0 * * * sudo /home/bradleytenuta/piplex/plex-run-cron.sh >> /home/bradleytenuta/piplex/logs/plex-run-cron.txt 2>&1
 #
 #
 
 # Mounts the external hard drive to the mnt folder.
-sudo mount -o uid=bradleytenuta,gid=plexgroup /dev/sda1 /home/bradleytenuta/mnt/ext_1
+mount -o uid=bradleytenuta,gid=plexgroup /dev/sda1 /home/bradleytenuta/mnt/ext_1
